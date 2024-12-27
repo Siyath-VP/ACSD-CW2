e2f5ddd25c8ac387c667154dc33338b014702ee1
Siyath-VP <siyathpandigama2003@gmail.com>
2024-12-28 00:23:02 +0530

    feat: Add FavoritesList component with drag-and-drop functionality and styling

---
+++ src/components/FavoritesList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function FavoritesList({ favourites, removeFavourite, clearFavourites, addFavourite }) {
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text/plain');
+    if (data) {
+      const prop = JSON.parse(data);
+      addFavourite(prop);
+    }
+  };
+
+  const allowDrop = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className="favorites-list" onDragOver={allowDrop} onDrop={handleDrop}>
+        {favourites.map(f => (
+          <div className="fav-item" key={f.id}
+            draggable={true}
+            onDragStart={(e) => e.dataTransfer.setData('text/plain', JSON.stringify(f))}
+          >
+            {f.shortDescription}
+            <button onClick={() => removeFavourite(f.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+      <button onClick={clearFavourites}>Clear All</button>
+    </div>
+  );
+}
