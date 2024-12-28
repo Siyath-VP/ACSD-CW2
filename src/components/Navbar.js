89716663f781fb064109ab728b05ab2f98999d5c
Siyath-VP <siyathpandigama2003@gmail.com>
2024-12-29 00:46:48 +0530

    feat: Added Footer and Navbar as separate components for reusability

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ toggleSidebar }) => {
+    return (
+        <nav>
+            <div style={{ fontSize: "1.8rem", fontWeight: "bold" }}>FindR</div>
+            <ul>
+                <li>
+                    <button onClick={toggleSidebar}>
+                        <FaSearch />
+                    </button>
+                </li>
+                <li>
+                    <Link to="/">Sell</Link>
+                </li>
+                <li>
+                    <Link to="#search">Contact us</Link>
+                </li>
+                <li>
+                    <Link to="/favorites">Favorites</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
