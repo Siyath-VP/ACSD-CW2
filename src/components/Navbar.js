e9769ecef5054a5ca0b8774fdb8ede1400713519
Siyath-VP <siyathpandigama2003@gmail.com>
2025-01-04 13:25:18 +0530

    fix: Navbar overlapping content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,27 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ toggleSidebar }) => {
-    return (
-        <nav>
-            <div style={{ fontSize: "1.8rem", fontWeight: "bold" }}>FindR</div>
-            <ul>
-                <li>
-                    <button onClick={toggleSidebar}>
-                        <FaSearch />
-                    </button>
-                </li>
-                <li>
-                    <Link to="/">Sell</Link>
-                </li>
-                <li>
-                    <Link to="#search">Contact us</Link>
-                </li>
-                <li>
-                    <Link to="/favorites">Favorites</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <div style={{ fontSize: "1.8rem", fontWeight: "bold" }}>FindR</div>
+      <ul>
+        <li>
+          <button onClick={toggleSidebar}>
+            <FaSearch />
+          </button>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="#search">Contact us</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
